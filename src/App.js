2dfc19fcf85789bba8988e33a55f6b3af3d6860e
Shreyas Bhumkar <shreyasbhumkar2@gmail.com>
2022-11-16 23:13:09 +0530

    - New Changes Applied (Added New Component - HookMouse & MouseContainer)   - Added Component HookMouse component to log the mouse position   - Added Component to Toggle visibility of MouseHook component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 // import logo from './logo.svg';
 import './App.css';
-import AddMovie from './component/AddMovie';
-import MovieList from './component/MovieList';
-import UpdateMovie from './component/UpdateMovie';
+// import AddMovie from './component/AddMovie';
+// import MovieList from './component/MovieList';
+// import UpdateMovie from './component/UpdateMovie';
+// import IconPlotting from './component/IconPlotting';
+// import HookMouse from './component/HookMouse';
+import MouseContainer from './component/MouseContainer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h3>Firestore Database with React</h3>
-      </header>
       <main>
-        <MovieList />
+        {/* <MovieList />
         <AddMovie />
-        <UpdateMovie />
+        <UpdateMovie /> */}
+        {/* <IconPlotting /> */}
+        <MouseContainer />
       </main>
     </div>
   );
